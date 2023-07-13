9fc4664a1a44174342a60dc165e632941cf75876
angeBerthe <seugbanangeberthe@gmail.com>
2023-07-13 14:44:09 +0000

    Dépôt travail

---
+++ Hopital_ARSTM/src/main/java/com/sati/controllers/TypeConsultationController.java
@@ -0,0 +1,123 @@
+package com.sati.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.component.commandbutton.CommandButton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.sati.model.TypeConsultation;
+import com.sati.service.Iservice;
+
+@Component
+@Scope("session")
+public class TypeConsultationController {
+
+	
+	@Autowired
+	Iservice service;
+	private TypeConsultation typeConsultation = new TypeConsultation();
+	private List<TypeConsultation> listTypeConsultation = new ArrayList<TypeConsultation>();
+	private TypeConsultation selectedTypeConsultation;
+	
+	
+	private CommandButton btnEnregistrer = new CommandButton();
+	private CommandButton btnModifier = new CommandButton();
+	private CommandButton btnSupprimer = new CommandButton();
+	
+	@PostConstruct
+	public void initialiser() {
+		this.btnModifier.setDisabled(true);
+		genererCodeTypeConsultation();
+	}
+	
+	public String genererCodeTypeConsultation() {
+		String prefix="";
+		int nbEnregistrement = this.service.getObjects("TypeConsultation").size();
+		if(nbEnregistrement < 10)
+			prefix = "TC00" ;
+		if ((nbEnregistrement >= 10) && (nbEnregistrement < 100)) 
+			prefix = "TC0" ;
+		if (nbEnregistrement > 100) 
+			prefix = "TC" ;
+		return new String(prefix+(nbEnregistrement+1));
+	}
+	public void enregistrer() {
+		
+		typeConsultation.setCodeTypeConsultation(genererCodeTypeConsultation());
+		service.addObject(typeConsultation);
+		annuler();
+		this.info("Enregistrement effectué avec succès!");
+	}
+	
+	public void selectionnerLigne() {
+		this.typeConsultation = selectedTypeConsultation;
+		this.btnEnregistrer.setDisabled(true);
+		this.btnModifier.setDisabled(false);
+	}
+	public void modifier() {
+		service.updateObject(typeConsultation);
+		annuler();
+		this.info("Modification effectuée avec succès!");
+	}
+	
+	public void supprimer() {
+		service.deleteObject(typeConsultation);
+		annuler();
+		this.info("Suppression effectuée avec succès!");
+	}
+	
+	public void annuler() {
+		typeConsultation.setCodeTypeConsultation(null);
+		typeConsultation.setCodeTypeConsultation(null);
+	}
+	public void info(String monMessage) {
+		FacesContext.getCurrentInstance().addMessage((String) null, new FacesMessage(FacesMessage.SEVERITY_INFO, monMessage,null ));
+	}
+	public TypeConsultation getTypeConsultation() {
+		return typeConsultation;
+	}
+	public void setTypeConsultation(TypeConsultation typeConsultation) {
+		this.typeConsultation = typeConsultation;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<TypeConsultation> getListTypeConsultation() {
+		listTypeConsultation = service.getObjects("TypeConsultation");
+		System.out.println("===========Taille de la liste:"+listTypeConsultation.size());
+		return listTypeConsultation;
+	}
+	public void setListTypeConsultation(List<TypeConsultation> listTypeConsultation) {
+		this.listTypeConsultation = listTypeConsultation;
+	}
+	public TypeConsultation getSelectedTypeConsultation() {
+		return selectedTypeConsultation;
+	}
+	public void setSelectedTypeConsultation(TypeConsultation selectedTypeConsultation) {
+		this.selectedTypeConsultation = selectedTypeConsultation;
+	}
+	public CommandButton getBtnEnregistrer() {
+		return btnEnregistrer;
+	}
+	public void setBtnEnregistrer(CommandButton btnEnregistrer) {
+		this.btnEnregistrer = btnEnregistrer;
+	}
+	public CommandButton getBtnModifier() {
+		return btnModifier;
+	}
+	public void setBtnModifier(CommandButton btnModifier) {
+		this.btnModifier = btnModifier;
+	}
+	public CommandButton getBtnSupprimer() {
+		return btnSupprimer;
+	}
+	public void setBtnSupprimer(CommandButton btnSupprimer) {
+		this.btnSupprimer = btnSupprimer;
+	}
+}
---
+++ Hopital_ARSTM/src/main/java/com/sati/controllers/UserAuthenticationController.java
@@ -0,0 +1,145 @@
+package com.sati.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.component.commandbutton.CommandButton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.sati.model.UserAuthentication;
+import com.sati.model.UserAuthorization;
+import com.sati.service.Iservice;
+
+@Component
+@Scope("session")
+public class UserAuthenticationController {
+	
+	@Autowired
+	Iservice service;
+	private String role;
+	private UserAuthentication userAuthentication = new UserAuthentication();
+	private List<UserAuthentication> listUserAuthentication = new ArrayList<UserAuthentication>();
+	private UserAuthentication selectedUserAuthentication;
+	private UserAuthorization userAuthorization = new UserAuthorization();
+	
+	private CommandButton btnEnregistrer = new CommandButton();
+	private CommandButton btnModifier = new CommandButton();
+	private CommandButton btnSupprimer = new CommandButton();
+	
+	
+	@PostConstruct
+	public void initialiser() {
+		this.btnModifier.setDisabled(true);
+	}
+	
+	public void enregistrer() {
+		userAuthentication.setEnabled(true);
+		service.addObject(userAuthentication);
+		userAuthorization.setUserAuthentication(userAuthentication);
+		userAuthorization.setRole(role);
+		service.addObject(userAuthorization);
+		annuler();
+		this.info("Enregistrement effectué avec succès!");
+	}
+	
+	
+	public void info(String monMessage) {
+		FacesContext.getCurrentInstance().addMessage((String) null, new FacesMessage(FacesMessage.SEVERITY_INFO, monMessage,null ));
+	}
+	
+	public void annuler() {
+		userAuthentication.setUsername(null);
+		userAuthentication.setPassword(null);
+		userAuthentication.setNom(null);
+		userAuthentication.setEmail(null);
+		userAuthentication.setPrenoms(null);
+		userAuthentication.setTelephone(null);
+		userAuthorization.setRole(null);
+	}
+	
+	public void modifier() {
+		service.updateObject(userAuthentication);
+		annuler();
+		this.info("Modification effectuée avec succès!");
+	}
+	
+	public void supprimer() {
+		service.deleteObject(userAuthentication);
+		annuler();
+		this.info("Suppression effectuée avec succès!");
+	}
+	public void selectionnerLigne() {
+		this.userAuthentication = this.selectedUserAuthentication;
+		this.btnEnregistrer.setDisabled(true);
+		this.btnModifier.setDisabled(false);
+	}
+	public UserAuthentication getUserAuthentication() {
+		return userAuthentication;
+	}
+	public void setUserAuthentication(UserAuthentication userAuthentication) {
+		this.userAuthentication = userAuthentication;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<UserAuthentication> getListUserAuthentication() {
+		listUserAuthentication = service.getObjects("UserAuthentication");
+		System.out.println("==========Taille de la liste:"+listUserAuthentication.size());
+		return listUserAuthentication;
+	}
+	public void setListUserAuthentication(List<UserAuthentication> listUserAuthentication) {
+		this.listUserAuthentication = listUserAuthentication;
+	}
+	public UserAuthentication getSelectedUserAuthentication() {
+		return selectedUserAuthentication;
+	}
+	public void setSelectedUserAuthentication(UserAuthentication selectedUserAuthentication) {
+		this.selectedUserAuthentication = selectedUserAuthentication;
+	}
+	public UserAuthorization getUserAuthorization() {
+		return userAuthorization;
+	}
+	public void setUserAuthorization(UserAuthorization userAuthorization) {
+		this.userAuthorization = userAuthorization;
+	}
+	public CommandButton getBtnEnregistrer() {
+		return btnEnregistrer;
+	}
+	public void setBtnEnregistrer(CommandButton btnEnregistrer) {
+		this.btnEnregistrer = btnEnregistrer;
+	}
+
+
+	public CommandButton getBtnModifier() {
+		return btnModifier;
+	}
+
+
+	public void setBtnModifier(CommandButton btnModifier) {
+		this.btnModifier = btnModifier;
+	}
+
+
+	public CommandButton getBtnSupprimer() {
+		return btnSupprimer;
+	}
+
+
+	public void setBtnSupprimer(CommandButton btnSupprimer) {
+		this.btnSupprimer = btnSupprimer;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
